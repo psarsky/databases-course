b8f33d3cce89eec30fd170b55165b21069c83a58
Jakub Psarski <psarski@student.agh.edu.pl>
2025-05-14 13:45:31 +0200

    finished part I

---
+++ EntityFramework/Product.cs
@@ -0,0 +1,6 @@
+public class Product
+{
+    public int ProductID { get; set; }
+    public String? ProductName { get; set; }
+    public int UnitsInStock { get; set; }
+}
--- EntityFramework/Program.cs
+++ EntityFramework/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+ProdContext prodContext = new ProdContext();
+Console.WriteLine("Enter product name:");
+String? prodName = Console.ReadLine();
+Product product = new Product {ProductName = prodName};
+prodContext.Products.Add(product);
+prodContext.SaveChanges();
+
+var query = from prod in prodContext.Products
+            select prod.ProductName;
+
+Console.WriteLine("\nProducts in the database:");
+
+foreach (var pName in query)
+{
+    Console.WriteLine(pName);
+}
